4ea8aa5f2a8fffbde6d225a2ce0cf085458d2d04
Tom Dooner <tom.dooner@brigade.com>
2015-12-25 23:08:05 -0500

    Add debug

--- src/AristotleSolver.java
+++ src/AristotleSolver.java
@@ -1,6 +1,7 @@
 import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.constraints.IntConstraintFactory;
 import org.chocosolver.solver.search.strategy.IntStrategyFactory;
+import org.chocosolver.solver.trace.Chatterbox;
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.solver.variables.VariableFactory;
 
@@ -76,6 +77,8 @@ public class AristotleSolver {
 
         // 5. Launch the resolution process
         solver.findSolution();
+
+        Chatterbox.printStatistics(solver);
 
         // 6. Print the answer
         System.out.println("From top left to bottom right:");
